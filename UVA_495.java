76e80f658322ab490b5f0a4870744aba0031f88d
Ezio112 <44439644+Ezio112@users.noreply.github.com>
2020-09-28 20:06:42 +0530

    Update UVA_495.java

--- UVA_495.java
+++ UVA_495.java
@@ -6,11 +6,11 @@ public class Main{
 		Scanner sc=new Scanner(System.in);
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
-		long dp[]=new long[5005];
-		dp[0]=0;
-		dp[1]=1;
+		BigInteger dp[]=new BigInteger[5005];
+		dp[0]=new BigInteger("0");
+		dp[1]=new BigInteger("1");
 		for(int i=2;i<=5000;++i)
-			dp[i]=dp[i-1]+dp[i-2];
+			dp[i]=dp[i-1].add(dp[i-2]);
 		while(sc.hasNext()){
 			int n=sc.nextInt();
 			System.out.println("The Fibonacci number for "+n+" is "+dp[n]);
