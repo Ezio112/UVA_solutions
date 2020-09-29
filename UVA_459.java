b8dde5a85a0e41172b882c5f435d23a2d5ef2621
Ezio112 <44439644+Ezio112@users.noreply.github.com>
2020-09-29 12:20:57 +0530

    Create UVA_459.java

---
+++ UVA_459.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main {
+	static boolean[] visited;
+	static ArrayList<Integer>[] adj;
+
+	private static int ans() {
+		visited = new boolean[adj.length];
+		int count = 0;
+		for (int i = 0; i < adj.length; i++) {
+			if (!visited[i]) {
+				count++;
+				dfs(i);
+			}
+		}
+		return count;
+	}
+
+	private static void dfs(int x) {
+		visited[x] = true;
+		for (int i = 0; i < adj[x].size(); i++) {
+			if (!visited[adj[x].get(i)]) {
+				dfs(adj[x].get(i));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int testcase = scanner.nextInt();
+		scanner.nextLine();
+		scanner.nextLine();
+		for (int i = 0; i < testcase; i++) {
+			char max = scanner.nextLine().charAt(0);
+			int num_of_node = max - 'A' + 1;
+			adj = (ArrayList<Integer>[]) new ArrayList[num_of_node];
+			for (int j = 0; j < num_of_node; j++) {
+				adj[j] = new ArrayList<Integer>();
+			}
+			String edge;
+			while (scanner.hasNextLine() && !(edge = scanner.nextLine()).equals("")) {
+				int x, y;
+				x = edge.charAt(0) - 'A' + 1;
+				y = edge.charAt(1) - 'A' + 1;
+				adj[x - 1].add(y - 1);
+				adj[y - 1].add(x - 1);
+			}
+			System.out.println(ans());
+			if (i != testcase - 1)
+				System.out.println();
+		}
+	}
+}
