bc0d3cedf307b1762a4bb2510b73ac6de13be042
Ezio112 <44439644+Ezio112@users.noreply.github.com>
2020-10-05 21:42:18 +0530

    Update UVA_10302.java

--- UVA_10302.java
+++ UVA_10302.java
@@ -2,24 +2,15 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 public class Main{
-	static int counter(int n){
-		int i=1,count=0;
-		while(i*i<n){
-			if(n%i==0)	count+=2;
-			++i;
-		}
-		if(n%(i*i)==0)	++count;
-		return count;
-	}
-			
 	public static void main(String args[])	throws IOException{
 		Scanner sc=new Scanner(System.in);
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		
-		while(true){
-			int n=Integer.parseInt(br.readLine());
+		while(sc.hasNext()){
+			long n=sc.nextLong();
 			long s=(n*n+n)/2;
 			System.out.println(s*s);
 		}
 	}
 }
+	
